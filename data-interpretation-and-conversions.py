825ad0e76e497a8af92dfd7bd1d7cbff4fdbb0d6
gturner24 <91153703+gturner24@users.noreply.github.com>
2021-10-06 12:03:02 -0500

    Update data-interpretation-and-conversions.py

--- data-interpretation-and-conversions.py
+++ data-interpretation-and-conversions.py
@@ -19,12 +19,12 @@ for i in range(len(bytesList)):
 
 
 # Interpreting destination and source MAC addresses
-destinationMACAddress = []
+destinationMACAddressLIST = []
 for x in range(6):
-    destinationMACAddress.append(bytesList[x])
-sourceMACAddress = []
+    destinationMACAddressLIST.append(bytesList[x])
+sourceMACAddressLIST = []
 for y in range(6, 12):
-    sourceMACAddress.append(bytesList[y])
+    sourceMACAddressLIST.append(bytesList[y])
 
 
 # Interpreting Ethernet Type
@@ -45,3 +45,37 @@ elif typeBytes[0] == "86":
         ethernetType = "Unknown"
 else:
     ethernetType = "Unknown"
+
+
+# Interpreting protocol number
+protocolNumber = bytesList[23]
+if protocolNumber == "01":
+    encapsulatedProtocol = "Internet Control Message (ICMP)"
+elif protocolNumber == "02":
+    encapsulatedProtocol = "Internet Group Management (IGMP)"
+elif protocolNumber == "06":
+    encapsulatedProtocol = "Transmission Control Protocol (TCP)"
+elif protocolNumber == "11":
+    encapsulatedProtocol = "Transport User Datagram (UDP)"
+elif protocolNumber == "58":
+    encapsulatedProtocol = "Routing Protocol EIGRP"
+elif protocolNumber == "58":
+    encapsulatedProtocol = "Routing Protocol OSPF"
+else:
+    encapsulatedProtocol = "Unknown"
+
+
+# Interpreting IP addresses
+sourceIPAddressLIST = []
+for i in range(26, 30):
+    sourceIPAddressLIST.append(convertedBytes[i])
+sIP = sourceIPAddressLIST
+sourceIPAddress = (str(sIP[0]) + '.' + str(sIP[1]) + '.' + str(sIP[2]) + '.' + str(sIP[3]))
+
+destinationIPAddressLIST = []
+for i in range(30, 34):
+    destinationIPAddressLIST.append(convertedBytes[i])
+dIP = destinationIPAddressLIST
+destinationIPAddress = (str(dIP[0]) + '.' + str(dIP[1]) + '.' + str(dIP[2]) + '.' + str(dIP[3]))
+
+
